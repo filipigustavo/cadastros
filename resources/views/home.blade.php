1c353ca817227e51652006aeddbe26f18ac5fbad
GRPCOM <grpcom@311PLAT046382D.local>
2017-06-07 17:12:00 -0300

    api authentication - parcial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,6 +25,10 @@
                       <a href="{{route('criar_categoria')}}">Criar Categoria</a>
                     </li>
                   </ul>
+
+                  <passport-clients></passport-clients>
+                  <passport-authorized-clients></passport-authorized-clients>
+                  <passport-personal-access-tokens></passport-personal-access-tokens>
                 </div>
             </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,7 @@
 <div class="container">
   <div class="row">
     <div class="col-sm-12">
-
       <a href="/login">Login</a>
-
     </div>
   </div>
 </div>
