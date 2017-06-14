6f600111a0892c6d3013844783e738e50e5a1163
GRPCOM <grpcom@311PLAT046382D.local>
2017-06-14 17:17:01 -0300

    middleware group

--- routes/api.php
+++ routes/api.php
@@ -13,33 +13,37 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/',function(){
   return 'not this time';
 });
 
-// Objetos JSON
-Route::get('/objetos', function(){
-  $objetos = \App\Objeto::all()->load('categoria');
-  return response()->json($objetos);
-});
-Route::get('/objetos/{id}', function($id){
-  $objeto = \App\Objeto::find($id)->load('categoria');
-  return response()->json($objeto);
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
 });
 
-// Categorias JSON
-Route::get('/categorias', function(Request $request){
-  // if($request->input('callback')){
-    $categorias = \App\Categoria::all(); // ->load('objetos')
-    return response()->json($categorias); // ->withCallback($request->input('callback'));
-  // }
-  // return;
-})->middleware(['cors', 'auth:api']);
-Route::get('/categorias/{id}', function($id){
-  $categoria = \App\Categoria::find($id)->load('objetos');
-  return response()->json($categoria);
+Route::group(['middleware' => ['cors', 'auth:api']], function(){
+  // Objetos JSON
+  Route::get('/objetos', function(){
+    $objetos = \App\Objeto::all()->load('categoria');
+    return response()->json($objetos);
+  });
+
+  Route::get('/objetos/{id}', function($id){
+    $objeto = \App\Objeto::find($id)->load('categoria');
+    return response()->json($objeto);
+  });
+
+  // Categorias JSON
+  Route::get('/categorias', function(Request $request){
+    // if($request->input('callback')){
+      $categorias = \App\Categoria::all(); // ->load('objetos')
+      return response()->json($categorias); // ->withCallback($request->input('callback'));
+    // }
+    // return;
+  });
+
+  Route::get('/categorias/{id}', function($id){
+    $categoria = \App\Categoria::find($id)->load('objetos');
+    return response()->json($categoria);
+  });
 });
